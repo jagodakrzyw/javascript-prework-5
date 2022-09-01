0cccaf8071dfb4ff0100176e40874ee1a4fab21e
Jagoda Krzywdzińska <jagodakrzywdzinska@gmail.com>
2022-09-01 18:14:56 +0200

    Reorganize functions in script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+  } else if (argMoveId == 2){
+    return 'papier';
+  } else if (argMoveId == 3){
+    return 'nożyce'; 
+  } 
+
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+  if((argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+  (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') ||
+  (argComputerMove == 'papier' && argPlayerMove == 'nożyce')) {
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == argPlayerMove){
+    printMessage('Remis!');
+  } else {
+    printMessage('Tym razem przegrywasz!')
+  }
+}
+
 function playGame(playerInput){
   clearMessages();
-
-  function getMoveName(argMoveId){
-    if(argMoveId == 1){
-      return 'kamień';
-    } else if (argMoveId == 2){
-      return 'papier';
-    } else if (argMoveId == 3){
-      return 'nożyce'; 
-    } 
-
-      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-      return 'nieznany ruch';
-    }
 
   let randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('Wylosowana liczba to: ' + randomNumber);
@@ -22,18 +34,6 @@ function playGame(playerInput){
 
   let playerMove = getMoveName(playerInput);
   printMessage('Twój ruch to: ' + playerMove);
-
-  function displayResult(argComputerMove, argPlayerMove){
-    if((argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
-    (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') ||
-    (argComputerMove == 'papier' && argPlayerMove == 'nożyce')) {
-      printMessage('Ty wygrywasz!');
-    } else if (argComputerMove == argPlayerMove){
-      printMessage('Remis!');
-    } else {
-      printMessage('Tym razem przegrywasz!')
-    }
-  }
 
   console.log('moves:', computerMove, playerMove);
 
